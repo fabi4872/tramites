f27cc5a2d82081799e64a4259f882de199a535a5
Fabian Salomone <fabi4872@gmail.com>
2023-10-17 08:39:58 -0300

    Nuevos cambios

--- src/pages/tramites/Tramite/components/GestorMotivos/ListadoMotivosOv.jsx
+++ src/pages/tramites/Tramite/components/GestorMotivos/ListadoMotivosOv.jsx
@@ -1,4 +1,4 @@
-import { Accordion, Button, Col, Container, Row, Toast } from 'react-bootstrap';
+import { Accordion, Button, Col, Container, Form, Row, Toast } from 'react-bootstrap';
 import PropTypes from "prop-types";
 import TablaAsociaciones from './TablaAsociaciones';
 import ListadoMotivosSubmotivos from './ListadoMotivosSubmotivos';
@@ -7,9 +7,12 @@ import HandleSweetAlert from '../shared/AlertModal';
 import { ImSwitch } from 'react-icons/im';
 import { BsTrash } from 'react-icons/bs';
 import { editMotivoOv } from '../../../../../modules/motivosOv';
+import { useEffect, useState } from 'react';
+import { BiEditAlt, BiListCheck } from 'react-icons/bi';
 
 const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {    
     const dispatch = useDispatch();
+    const [edicion, setEdicion] = useState([]);
 
     // Handlers
     const handleGetAsociacionesByMotivoOv = (idMotivoOv) => {
@@ -27,6 +30,60 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
         dispatch(editMotivoOv({ motivoOv: updatedMotivoOv, codigoRubro }));
     };
 
+    const handleOnClickEdit = (numeroItem, valorNuevo) => {
+        console.log(numeroItem, valorNuevo);
+        setEdicion((currentData) => {
+            const updatedData = [...currentData];
+            updatedData[numeroItem] = {
+                ...updatedData[numeroItem],
+                esEdicion: valorNuevo,
+            };
+            return updatedData;
+        });
+    };
+
+    const handleChange = (e, numeroItem) => {
+        const { value } = e.target;
+        setEdicion((currentData) => {
+            const updatedData = [...currentData];
+            updatedData[numeroItem] = {
+                ...updatedData[numeroItem],
+                input: value,
+            };
+            return updatedData;
+        });
+    }
+
+    const handleOnClickEditConfirm = (motivoOv, numeroItem, valorNuevo) => {
+        console.log(numeroItem);
+        const updatedMotivoOv = { ...motivoOv };
+        updatedMotivoOv.descripcion = valorNuevo.trim();
+        motivosOv = [
+            ...motivosOv.filter((elemento) => elemento.id !== motivoOv.id),
+            updatedMotivoOv,
+        ];
+        dispatch(editMotivoOv({ motivoOv: updatedMotivoOv, codigoRubro }));
+        setEdicion((currentData) => {
+            const updatedData = [...currentData];
+            updatedData[numeroItem] = {
+                ...updatedData[numeroItem],
+                esEdicion: false,
+                input: valorNuevo,
+            };
+            return updatedData;
+        });
+    };
+
+    // useEffect
+    useEffect(() => {
+        setEdicion(
+            motivosOv.map((elemento) => ({
+                esEdicion: false,
+                input: elemento.descripcion,
+            }))
+        );
+    }, [motivosOv]);
+
     return (
         <>
             <Container style={{ marginBottom: "6rem" }}>
@@ -42,6 +99,50 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
                                         mt={3}
                                         style={{ position: "relative" }}
                                     >
+                                        {edicion[i]?.esEdicion && (
+                                            <Col
+                                                xs={10}
+                                                lg={6}
+                                                style={{
+                                                    position: "absolute",
+                                                    top: "-3.35rem",
+                                                    zIndex: "99",
+                                                }}
+                                                className="mt-2 d-flex justify-content-center align-items-center"
+                                            >
+                                                <Form.Control
+                                                    type="text"
+                                                    id={`descripcion${m.id}`}
+                                                    aria-describedby="descripcion nueva consultar por"
+                                                    value={edicion[i]?.input}
+                                                    onChange={(e) =>
+                                                        handleChange(e, i)
+                                                    }
+                                                />
+                                                <Button
+                                                    variant="default"
+                                                    className="repositorio-icon-button"
+                                                    onClick={() =>
+                                                        HandleSweetAlert(
+                                                            () =>
+                                                                handleOnClickEditConfirm(
+                                                                    m,
+                                                                    i,
+                                                                    edicion[i]
+                                                                        ?.input
+                                                                ),
+                                                            "La descripción de etiqueta ha sido guardada satisfactoriamente",
+                                                            true
+                                                        )
+                                                    }
+                                                >
+                                                    <BiListCheck
+                                                        className="repositorio-icon-green"
+                                                        size={25}
+                                                    />
+                                                </Button>
+                                            </Col>
+                                        )}
                                         <div style={{ position: "relative" }}>
                                             <Row
                                                 style={{ margin: "0 .5rem" }}
@@ -102,6 +203,25 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
                                             >
                                                 <Button
                                                     variant="default"
+                                                    className="btn repositorio-icon-margin repositorio-icon-button"
+                                                    onClick={() =>
+                                                        handleOnClickEdit(
+                                                            i,
+                                                            !edicion[i]?.esEdicion
+                                                        )
+                                                    }
+                                                    disabled={
+                                                        !m.sn_activo ||
+                                                        edicion[i]?.esEdicion
+                                                    }
+                                                >
+                                                    <BiEditAlt
+                                                        className="repositorio-icon-yellow"
+                                                        size={25}
+                                                    />
+                                                </Button>
+                                                <Button
+                                                    variant="default"
                                                     className="btn repositorio-icon-button"
                                                     onClick={() =>
                                                         HandleSweetAlert(
@@ -133,7 +253,6 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
                                                 </Button>
                                             </div>
                                         </div>
-
                                         <ListadoMotivosSubmotivos
                                             motivoOv={m}
                                             index={i}
@@ -143,7 +262,6 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
                                             }
                                             codigoRubro={codigoRubro}
                                         />
-
                                         <TablaAsociaciones
                                             motivoOv={m}
                                             motivosRepositorioAsociados={handleGetAsociacionesByMotivoOv(
@@ -152,12 +270,13 @@ const ListadoMotivosOv = ({ motivosOv, motivosRepositorio, codigoRubro }) => {
                                             codigoRubro={codigoRubro}
                                             activo={m.sn_activo}
                                         />
-
                                         <Container>
                                             <Row className="mt-4 mb-3 justify-content-center d-flex text-center">
                                                 <Col className="mb-2" xs={12}>
                                                     <Button
-                                                        style={{ fontWeight: "bold" }}
+                                                        style={{
+                                                            fontWeight: "bold",
+                                                        }}
                                                         variant="outline-danger"
                                                         onClick={() =>
                                                             HandleSweetAlert(
